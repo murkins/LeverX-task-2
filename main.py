a004701b9ed1754953eeeb5593d09c3f6ad61295
murkins <kateazhevskaya@gmail.com>
2021-12-16 10:18:26 +0300

    Updated the file in accordance with the suggestions

--- main.py
+++ main.py
@@ -3,51 +3,76 @@ from functools import total_ordering
 
 @total_ordering
 class Version:
+
+    @staticmethod
+    def subversion_to_common_format(s):
+        """ Converting subversion to common format """
+        s = s.lower()
+        s = s.replace("alpha", "a")
+        s = s.replace("beta", "b")
+        s = s.replace("-", "")
+
+        return s
+
+    @staticmethod
+    def split_subversion_into_digits_and_letters(subvers):
+        digits = []
+        letters = []
+
+        for c in subvers:
+            if c in string.digits:
+                digits.append(c)
+            elif c in string.ascii_lowercase:
+                letters.append(c)
+
+        return digits, letters
+
+    def append_to_numbers_and_ratings(self, digits, letters):
+        # append number
+        if digits:
+            number_string = "".join(digits)
+            self.numbers.append(int(number_string))
+        else:
+            self.numbers.append(0)
+
+        # append rating
+        letters_string = "".join(letters)
+        rating = self.rating_mapping[letters_string]
+
+        self.ratings.append(rating)
+
     def __init__(self, version):
         self.numbers = []
         self.ratings = []
 
+        self.rating_mapping = {
+            "a": 1,
+            "b": 2,
+            "rc": 3,
+            "": 4,
+            "r": 5
+        }
+
         subversions = version.split('.')
         for subvers in subversions:
-            subvers = subvers.lower()
-            subvers = subvers.replace("alpha", "a")
-            subvers = subvers.replace("beta", "b")
-            subvers = subvers.replace("-", "")
+            # converting subversion to common format
+            subvers = Version.subversion_to_common_format(subvers)
 
-            digits = []
-            letters = []
+            # splitting subversion into digits and letters
+            digits, letters = Version.split_subversion_into_digits_and_letters(subvers)
 
-            for c in subvers:
-                if c in string.digits:
-                    digits.append(c)
-                elif c in string.ascii_lowercase:
-                    letters.append(c)
-
-            # append number
-            if len(digits) != 0:
-                number_string = "".join(digits)
-                self.numbers.append(int(number_string))
-            else:
-                self.numbers.append(0)
-
-            # append rating
-            letters_string = "".join(letters)
-            rating = None
-            if 'a' in letters_string:
-                rating = 1
-            elif 'b' in letters_string:
-                rating = 2
-            elif 'rc' in letters_string:
-                rating = 3
-            elif len(letters_string) == 0:
-                rating = 4
-            elif 'r' in letters_string:
-                rating = 5
-            assert rating is not None, "Version is incorrect"
-            self.ratings.append(rating)
+            self.append_to_numbers_and_ratings(digits, letters)
 
     def __lt__(self, other):
-        min_size = min(len(self.numbers), len(other.numbers))
+        longer = None
+        if len(self.numbers) > len(other.numbers):
+            longer = self
+            min_size = len(other.numbers)
+            self_is_longer = True
+        else:
+            longer = other
+            min_size = len(self.numbers)
+            self_is_longer = False
 
         for i in range(min_size):
             if self.numbers[i] < other.numbers[i]:
@@ -62,18 +87,37 @@ class Version:
                 else:
                     continue
 
-        if len(self.numbers) < len(other.numbers):
-            return True
+        for i in range(min_size, len(longer.numbers)):
+            if longer.numbers[i] > 0:
+                return not self_is_longer
+            elif longer.numbers[i] < 0:
+                return self_is_longer
+            else:
+                if longer.ratings[i] > 4:
+                    return not self_is_longer
+                elif longer.ratings[i] < 4:
+                    return self_is_longer
+                else:
+                    continue
 
         return False
 
 
     def __eq__(self, other):
-        if len(self.numbers) != len(other.numbers):
-            return False
+        longer = None
+        if len(self.numbers) > len(other.numbers):
+            longer = self
+            min_size = len(other.numbers)
+        else:
+            longer = other
+            min_size = len(self.numbers)
 
-        for i in range(len(self.numbers)):
-            if (self.numbers[i] != other.numbers[i]) or (self.ratings[i] != other.ratings[i]):
+        for i in range(min_size):
+            if self.numbers[i] != other.numbers[i] or self.ratings[i] != other.ratings[i]:
+                return False
+
+        for i in range(min_size, len(longer.numbers)):
+            if longer.numbers[i] != 0 or longer.ratings[i] != 4:
                 return False
 
         return True
@@ -86,6 +130,8 @@ def main():
         ("1.1.0-alpha", "1.2.0-alpha.1"),
         ("1.0.1b", "1.0.10-alpha.beta"),
         ("1.0.0-rc.1", "1.0.0"),
+        ("1", "1.0.0r"),
+        ("1.0.0a", "1")
     ]
 
     for version_1, version_2 in to_test:
@@ -94,6 +140,8 @@ def main():
         assert Version(version_2) != Version(version_1), "neq failed"
 
 
-if __name__ == "__main__":
-     main()
 
+if __name__ == "__main__":
+    main()
+
+    print(Version("1.0.0") == Version("1"))
